80b9bf2c02ca0d242d414a6bdb44b512f54587b7
Brandon Pinzon <brandonpinzont@gmail.com>
2025-01-09 16:54:28 -0500

    Add hero image

--- front/src/components/MainHome.tsx
+++ front/src/components/MainHome.tsx
@@ -1,22 +1,39 @@
+import Link from "next/link";
 import React from "react";
+import { AiOutlineArrowDown } from "react-icons/ai";
 
 const MainHome = () => {
   return (
     <section
       id="Main"
-      className="flex flex-col min-h-screen  w-full items-center justify-center"
+      className="flex flex-col min-h-screen  w-full items-center justify-start pt-36 "
     >
-      <h1 className="text-[#b7c9cd] font-thin text-5xl mb-6 md:text-7xl md:mb-8  lg:text-8xl lg:mb-10">
-        Muebles Casa River
-      </h1>
-      <h1 className="text-[#667528] font-thin text-4xl mb-6 md:text-5xl md:mb-8 lg:text-6xl lg:mb-10">
-        Comodamente Elegante
-      </h1>
-      <p className="text-[#b7c9cd] font-thin text-md pb-10 md:text-lg md:mb-14 lg:text-xl lg:mb-16">
-        Conoce nuestra amplia variedad de muebles para tu hogar, diseñados con los mejores materiales, para que puedas disfrutar de un ambiente acogedor y elegante.
-      </p>
-      
-      <button></button>
+      <div className="relative flex flex-col mb-20 py-36 w-full items-center justify-center bg-[url('/assets/hero_.webp')] bg-cover bg-bottom ">
+      <div className="absolute inset-0 bg-black/70 "></div>
+        <h1 className="text-[#ffba80] font-normal font-tenor text-5xl mb-6 md:text-7xl md:mb-8  lg:text-8xl lg:mb-10 relative z-10">
+          Muebles Casa River
+        </h1>
+        <p
+          className="text-[#c6c0e0] w-full p-4 lg:w-[50%] font-normal text-lg text-center md:text-lg  lg:text-2xl relative z-10"
+        >
+          Conoce nuestra amplia variedad de muebles para tu hogar, diseñados con
+          los mejores materiales, para que puedas disfrutar de un ambiente
+          acogedor y elegante.
+        </p>
+      </div>
+      <Link href='#Products' >
+      <button className="flex flex-row px-6 h-14 w-auto gap-2 text-base lg:text-2xl font-semibold items-center justify-center text-[#ffba80] shadow-lg  shadow-[#59521e] bg-[#8c8341] hover:bg-[#736a27] rounded-xl transition ease-in-out duration-300">
+        Ver productos{" "}
+        {
+          <AiOutlineArrowDown
+          size={32}
+          color="#ffba80"
+          strokeWidth={4}
+          className="p-0 m-0"
+          />
+        }
+      </button>
+        </Link>
     </section>
   );
 };
