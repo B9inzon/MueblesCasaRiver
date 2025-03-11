bb895a1c458ff65d847fa498a350be906b3f2971
Brandon Pinzon <brandonpinzont@gmail.com>
2025-03-11 12:14:14 -0500

    sequelize

--- back/src/database.js
+++ back/src/database.js
@@ -1,24 +1,48 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
+require('dotenv').config();
 
-dotenv.config();
-
-export const sequelize = new Sequelize(process.env.POSTGRES_URL, {
-  dialect: "postgres",
-  logging: false,
+export const development = {
+  url: process.env.POSTGRES_URL,
+  dialect: 'postgres',
   dialectOptions: {
-    ssl:{
+    ssl: {
       require: true,
       rejectUnauthorized: false
     }
   }
-});
-
-export const connectDB = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("🟢 Conexión con PostgreSQL establecida.");
-  } catch (error) {
-    console.error("🔴 Error al conectar con PostgreSQL:", error);
+};
+export const production = {
+  url: process.env.POSTGRES_URL,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
   }
 };
+// import { Sequelize } from "sequelize";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+// export const sequelize = new Sequelize(process.env.POSTGRES_URL, {
+//   dialect: "postgres",
+//   logging: false,
+//   dialectOptions: {
+//     ssl:{
+//       require: true,
+//       rejectUnauthorized: false
+//     }
+//   }
+// });
+
+// export const connectDB = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log("🟢 Conexión con PostgreSQL establecida.");
+//   } catch (error) {
+//     console.error("🔴 Error al conectar con PostgreSQL:", error);
+//   }
+// };
+
+
